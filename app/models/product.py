f0c6181a3687541dfcfa7eb96b44626b0dbda4a6
Jeddbasden <jeddbasden2@gmail.com>
2022-01-09 17:43:06 -0600

    Products model

---
+++ app/models/product.py
@@ -0,0 +1,15 @@
+from .db import db
+
+class Product(db.Model):
+  __tablename__ = 'products'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  price = db.Column(db.Integer, nullable=False)
+  description = db.Column(db.String)
+  imageUrl = db.Column(db.String)
+  rating = db.Column(db.Integer)
+  category = db.Column(db.String)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+  user = db.relationship('User', back_populates='products')
