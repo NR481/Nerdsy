15b2e7b1e8f4d2852675274026edb5ace2ce32d7
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-01-10 14:27:20 -0800

    merging product model

--- app/api/products.py
+++ app/api/products.py
@@ -6,5 +6,4 @@ product_routes = Blueprint('products', __name__)
 @product_routes.route('/')
 def index():
   products = Product.query.all()
-  print()
   return {product.id: [product.to_dict() for product in products]}
