105f268d4c3311a3ce3eba2fd3c33821173b058c
Gabriel Aspuria <gabrielmaspuria@gmail.com>
2022-01-10 14:25:02 -0800

    added to_dict to product model

--- app/api/products.py
+++ app/api/products.py
@@ -6,9 +6,5 @@ product_routes = Blueprint('products', __name__)
 @product_routes.route('/')
 def index():
   products = Product.query.all()
-  product_dict = []
-  for item in products:
-    product_dict.append(dict(item))
-
-  print({'products': [product.to_dict()['id'] for product in products]})
-  return {'products': [product.to_dict() for product in products]}
+  print()
+  return {product.id: [product.to_dict() for product in products]}
