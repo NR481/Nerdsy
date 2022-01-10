f9984b455ee01e9683eef152089ee5a1c02ee00c
zachery estefano <zacheryestefano@gmail.com>
2022-01-10 17:31:20 -0500

    refectored get products

--- app/api/products.py
+++ app/api/products.py
@@ -7,20 +7,22 @@ product_routes = Blueprint('products', __name__, url_prefix="/products")
 @product_routes.route('/')
 def index():
   products = Product.query.all()
+  return {'products': [product.to_dict() for product in products]}
 
-  all_products = [
-    {
-      "id": item.id,
-      "name": item.name,
-      "price": float(item.price),
-      "description": item.description,
-      "imageUrl": item.imageUrl,
-      "rating": item.rating,
-      "category": item.category,
-      "franchise": item.franchise,
-      "userId": item.userId
-    }
-    for item in products
-  ]
-  return jsonify( all_products )
+  # all_products = [
+  #   {
+  #     "id": item.id,
+  #     "name": item.name,
+  #     "price": float(item.price),
+  #     "description": item.description,
+  #     "imageUrl": item.imageUrl,
+  #     "rating": item.rating,
+  #     "category": item.category,
+  #     "franchise": item.franchise,
+  #     "userId": item.userId
+  #   }
+  #   for item in products
+  # ]
+  # return jsonify( all_products )
+
 
