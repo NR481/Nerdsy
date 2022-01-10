5680edb96dedec17184eada928e73ae13620066c
Jeddbasden <jeddbasden2@gmail.com>
2022-01-10 11:16:56 -0600

    changed total to deciaml numeric like price in products

--- app/models/shopping_cart.py
+++ app/models/shopping_cart.py
@@ -4,7 +4,7 @@ class ShoppingCart(db.Model):
   __tablename__ = "shopping_carts"
 
   id = db.Column(db.Integer, primary_key=True)
-  total = db.Column(db.Integer, nullable=False)
+  total = db.Column(db.Numeric(scale=2, precision=5, asdecimal=True), nullable=False)
   userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
   user = db.relationship('User', back_populates='shopping_cart')
