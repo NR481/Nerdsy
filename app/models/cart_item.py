91d508fd5863c490cc1a1a68065e50a0404d5007
Nick Rogers <nickjrogers@gmail.com>
2022-01-10 08:03:37 -0800

    corrected foreign key contraints in cart_items table

--- app/models/cart_item.py
+++ app/models/cart_item.py
@@ -4,8 +4,8 @@ class CartItem(db.Model):
   __tablename__ = 'cart_items'
 
   id = db.Column(db.Integer, primary_key=True)
-  productId = db.Column(db.Integer, db.ForeignKey('products'), nullable=False)
-  cartId = db.Column(db.Integer, db.ForeignKey('shopping_carts'), nullable=False)
+  productId = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+  cartId = db.Column(db.Integer, db.ForeignKey('shopping_carts.id'), nullable=False)
   quantity = db.Column(db.Integer, nullable=False)
 
   product = db.relationship('Product', back_populates='cart_items')
