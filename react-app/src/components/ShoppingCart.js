13f149aa7a98d7e48a34f93581c79d257d809abc
Jeddbasden <jeddbasden2@gmail.com>
2022-01-10 19:05:25 -0600

    shopping cart naive code

--- react-app/src/components/ShoppingCart.js
+++ react-app/src/components/ShoppingCart.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getShoppingCart } from "../store/products";
 
 const ShoppingCart = () => {
   const user = useSelector(state => state.session.user)
