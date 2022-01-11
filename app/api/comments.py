24f73408f998450c2900d1311ef9a02448bbf957
Nick Rogers <nickjrogers@gmail.com>
2022-01-11 13:22:09 -0800

    debugging edit comment

--- app/api/comments.py
+++ app/api/comments.py
@@ -22,3 +22,19 @@ def add_comment():
     db.session.add(comment)
     db.session.commit()
     return comment.to_dict()
+
+@comment_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_comment(id):
+  form = CommentForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  edited_comment = Comment.query.get(id)
+
+  if form.validate_on_submit():
+    edited_comment.comment = form.data['comment']
+    edited_comment.rating = form.data['rating']
+    edited_comment.productId = form.data['productId']
+    edited_comment.userId = form.data['userId']
+
+    db.session.commit()
+    return edited_comment.to_dict()
